c84a774c8d05e9d409c9d504a463f574d1a46fcc
Chekhlenkov <113024470+Chekhlenkov@users.noreply.github.com>
2023-06-12 16:57:18 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -17,7 +17,7 @@ class LoginView(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(rais_exception=True)
+        serializer.is_valid(raise_exception=True)
         user = serializer.save()
         login(request=request, user=user)
         return Response(serializer.data)
