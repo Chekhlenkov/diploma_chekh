4d1ba56115c2b5849b44b595c07463d343bb9ef3
Chekhlenkov <113024470+Chekhlenkov@users.noreply.github.com>
2023-06-16 10:53:42 +0300

    Update models.py

--- bot/models.py
+++ bot/models.py
@@ -2,8 +2,6 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.crypto import get_random_string
 
-from core.models import User
-
 USER = get_user_model()
 
 
